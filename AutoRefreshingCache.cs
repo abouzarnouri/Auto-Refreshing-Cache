dde243ad0f84571db4b528303e8413d8d95e8662
abouzarnouri <abouzarnouri@gmail.com>
2018-01-20 20:48:08 +0330

    Update AutoRefreshingCache.cs
    returning default(T) when exception occurs in calc method
    removed lock release on old object after new item inserted into cache
    using cacheName prefix to be able to use multiple instances of this cache

--- AutoRefreshingCache.cs
+++ AutoRefreshingCache.cs
@@ -1,23 +1,29 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Runtime.Caching;
 using System.Threading;
 using System.Threading.Tasks;
 
 namespace Hasin.Taaghche.Utilities
 {
-    public class AutoRefreshingCache<T> where T : class 
+    public class AutoRefreshingCache<T>
     {
         private readonly MemoryCache _cache;
         private readonly int _expireAfterSeconds;
         private readonly int _refreshAfterSeconds;
+        public readonly string CacheName;
 
-        public AutoRefreshingCache(int expireAfterSeconds, int refreshAfterSeconds)
+        // cacheName should be unique in the project and not containing # char
+        public AutoRefreshingCache(int expireAfterSeconds, int refreshAfterSeconds, string cacheName)
         {
             _cache = MemoryCache.Default;
             _expireAfterSeconds = expireAfterSeconds;
             _refreshAfterSeconds = refreshAfterSeconds;
+            CacheName = cacheName;
         }
 
+        // key, should not contain # char
         public T Get(string key, Func<T> calc)
         {
             var item = Get(key);
@@ -30,7 +36,7 @@ namespace Hasin.Taaghche.Utilities
             var refreshThreshold = DateTime.UtcNow.AddSeconds(-_refreshAfterSeconds);
             if (item.CalculationTime < refreshThreshold)
             {
-                if (Interlocked.Exchange(ref item.RefreshWorkers, 1) == 0)
+                if (Interlocked.Increment(ref item.RefreshWorkers) == 1)
                 {
                     Task.Run(() =>
                     {
@@ -41,22 +47,40 @@ namespace Hasin.Taaghche.Utilities
             return item.Value;
         }
 
+        public int CountExpiredElements(IEnumerable<string> keys)
+        {
+            return keys?.Where(k => Get(k) == null).Count() ?? 0;
+        }
+
         private void Refresh(string key, Func<T> calc)
         {
-            var data = calc();
+            T data;
+            try
+            {
+                data = calc();
+            }
+            catch (Exception ex)
+            {
+                data = default(T);
+            }
+            Inject(key, data);
+        }
+
+        public void Inject(string key, T data)
+        {
             var absoluteExpiration = DateTimeOffset.UtcNow.AddSeconds(_expireAfterSeconds);
             lock (_cache)
             {
-                _cache.Set(key, new CacheItemHolder(data), absoluteExpiration);
+                _cache.Set($"{CacheName}#{key}", new CacheItemHolder(data), absoluteExpiration);
             }
         }
 
         private CacheItemHolder Get(string key)
         {
-            return _cache.Get(key) as CacheItemHolder;
+            return _cache.Get($"{CacheName}#{key}") as CacheItemHolder;
         }
 
-        class CacheItemHolder
+        private class CacheItemHolder
         {
             internal readonly T Value;
             internal Int32 RefreshWorkers;
